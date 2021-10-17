b18a06e750f5baf370c01b441c419dd8ce806783
Pu Chen <shengnehs@qq.com>
2021-10-17 10:53:56 +0800

    'cnt_names'

---
+++ R/iso_cnt.R
@@ -0,0 +1,13 @@
+#' ISO code for countries
+#'
+#' A data containing the countrie' English and Chinese names
+#'
+#' @format A data frame with 247 rows and 9 variables
+#' \describe{
+#'   \item{iso2c}{2 bit code for countries}
+#'   \item{iso3c}{3 bit code for countries}
+#'   \item{number}{}
+#'   \item{country_en}{English names for countries}
+#'   \item{country_cn}{Chinese names for countries}
+#' }
+'iso_cnt'
