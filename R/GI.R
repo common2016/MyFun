8752f5f2868879c2f0421260f54d5f13668390ba
Pu Chen <shengnehs@qq.com>
2022-01-18 09:51:38 +0800

    'GI_one'

--- R/GI.R
+++ R/GI.R
@@ -6,6 +6,8 @@
 #' @param sig_u a cov matrix from VAR models. Note the order of variables in \code{sig_u}
 #' is same with one of \code{ma[[i]]}.
 #' @param imp_var a numerical scalor which specifies the impulse variable.
+#' @param unit \code{'sd'} is one standard devtiation shock which is default,
+#'  and \code{'one'} is one unit shock.
 #' @return a data frame, its row is variables and its coloumn is horizons.
 #' @import magrittr
 #' @export
@@ -13,13 +15,19 @@
 #' # see ar2ma function
 
 
-GI <- function(ma, sig_u, imp_var = 1){
+GI <- function(ma, sig_u, imp_var = 1, unit = 'sd'){
 
+  # the t period IRF
   GI_single <- function(ans, imp_var){
     ell <- matlab::zeros(nrow(ans),1)
     ell[imp_var] <- 1
     # GVAR toolbox p140, A.22
-    return((ans %*% sig_u %*% ell)/(as.numeric(sqrt(t(ell) %*% sig_u %*% ell))))
+    if (unit %in% 'sd'){
+      irf_single <- (ans %*% sig_u %*% ell)/(as.numeric(sqrt(t(ell) %*% sig_u %*% ell)))
+    } else if (unit %in% 'one'){
+      irf_single <- (ans %*% sig_u %*% ell)/(as.numeric(t(ell) %*% sig_u %*% ell))
+    }
+    return(irf_single)
   }
 
   irf <- lapply(ma, GI_single, imp_var = imp_var)
